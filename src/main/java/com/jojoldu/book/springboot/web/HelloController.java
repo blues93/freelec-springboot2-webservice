500b6961bca5cceffe00dda392a784e810b66c1d
blues93 <hellohaapy@gmail.com>
2021-01-03 23:58:26 +0900

    롬복 사용 Dto 테스트

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -1,12 +1,20 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController // 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 줍니다.
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/hello") // HTTP Method 인 Get 요청을 받을수 있는 API를 만들어 줍니다.
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
